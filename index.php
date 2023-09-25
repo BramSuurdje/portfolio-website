bc379cf39c6f986289659872249f2b3ca0cafcbb
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-09-25 14:22:25 +0200

    hello

--- index.php
+++ index.php
@@ -158,7 +158,7 @@
         <div class="footercontainer">
             <div class="footer-content">
                 <div class="footer-text">
-                    <h3>&copy 2023 Bram Suurd</h3>
+                    <h3>&copy; 2023 Bram Suurd</h3>
                 </div>
             </div>
         </div>

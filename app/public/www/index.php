cc085fc276ba16aac6e2914387459141751d82fd
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-10-02 10:39:59 +0200

    commit

--- app/public/www/index.php
+++ app/public/www/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="shortcut icon" href="img/favicon.svg" type="image/x-icon">
 </head>
+
 <body>
 
     <?php include("header.php"); ?>
@@ -21,10 +23,14 @@
                 <div class="hero-main">
                     <div class="hero-text">
                         <h1>Hi, I'm <b>Bram Suurd</b></h1>
-                        
-                        <p>Currently studying <i>Information Technology</i> at <a target="_blank" href="https://www.nhlstenden.com/en/courses/information-technology"><b>NHL Stenden <i class='bx bx-link-external' ></i></b></a></p>
-                        
-                        <span><a target="_blank" href="https://github.com/BramSuurdje"><i class='bx bxl-github'></i></a> <a target="_blank" href="https://www.linkedin.com/in/bramsuurd/"><i class='bx bxl-linkedin-square' ></i></a></span>
+
+                        <p>Currently studying <i>Information Technology</i> at <a target="_blank" href="https://www.nhlstenden.com/en/courses/information-technology"><b>NHL Stenden <i class='bx bx-link-external'></i></b></a></p>
+
+                        <span>
+                            <a target="_blank" href="https://github.com/BramSuurdje"><i class='bx bxl-github'></i></a> 
+                            <a target="_blank" href="https://www.linkedin.com/in/bramsuurd/"><i class='bx bxl-linkedin-square'></i></a>
+                            <a target="_blank" href="https://wa.me/+310621621560"><i class='bx bxl-whatsapp'></i></a>
+                        </span>
                     </div>
                     <div class="hero-img">
                         <img src="img/undraw_programming_re_kg9v.svg" alt="guy programming">
@@ -37,20 +43,21 @@
 
     <section id="about" class="about">
         <div class="container">
-        <div class="about-content">
-            <div class="about-img">
-                <img src="img/undraw_code_inspection_bdl7.svg" alt="About me">
+            <div class="about-content">
+                <div class="about-img">
+                    <img src="img/undraw_code_inspection_bdl7.svg" alt="About me">
+                </div>
+                <div class="about-text">
+                    <h3>About me</h3>
+                    <h4>An Information Technology Student
+                        <br>
+                        based in <a target="_blank" href="https://maps.app.goo.gl/o6vtFYk4X2Q7PLrD9">Emmen,The Netherlands <i class='bx bx-link-external'></i></a>
+                    </h4>
+                    <p>I live in the Netherlands, and I'm currently a student at <a href="https://nhlstenden.com/en" target="_blank">NHL Stenden <i class='bx bx-link-external'></i></a> in Emmen, where I'm studying Information Technology. What drew me to programming is the incredible potential it holds for creating and innovating. The world of coding offers boundless opportunities to bring new ideas to life.</p>
+                    <br>
+                    <p>Before coming to NHL Stenden, I was a student at <a href="https://www.drenthecollege.nl/english " target="_blank">Drenthe College <i class='bx bx-link-external'></i></a> in Emmen, where I studied "Expert IT System and Devices." This earlier experience provided me with a good foundation in technology. Now, at NHL Stenden, I'm excited to learn more and explore the world of Information Technology in greater depth.</p>
+                </div>
             </div>
-            <div class="about-text">
-                <h3>About me</h3>  
-                <h4>An Information Technology Student 
-                <br>
-                based in <a target="_blank" href="https://maps.app.goo.gl/o6vtFYk4X2Q7PLrD9">Emmen,The Netherlands <i class='bx bx-link-external' ></i></a></h4>
-                <p>I live in the Netherlands, and I'm currently a student at <a href="https://nhlstenden.com/en" target="_blank">NHL Stenden <i class='bx bx-link-external' ></i></a> in Emmen, where I'm studying Information Technology. What drew me to programming is the incredible potential it holds for creating and innovating. The world of coding offers boundless opportunities to bring new ideas to life</p>
-                <br>
-                <p>Before coming to NHL Stenden, I was a student at <a href="https://www.drenthecollege.nl/english " target="_blank">Drenthe College <i class='bx bx-link-external' ></i></a> in Emmen, where I studied "Expert IT System and Devices." This earlier experience provided me with a good foundation in technology. Now, at NHL Stenden, I'm excited to learn more and explore the world of Information Technology in greater depth.</p>
-            </div>
-        </div>
         </div>
     </section>
 
@@ -61,63 +68,63 @@
                 <h3>Here are all the projects that i've done</h3>
                 <div class="projects-grid">
                     <div class="grid-item">
-                    <div class="placeholder-image">
-                        <img src="/img/Cirle-Solutions.png" loading="lazy" alt="">
+                        <div class="placeholder-image">
+                            <img src="/img/Cirle-Solutions.png" loading="lazy" alt="">
                         </div>
-                            <div class="project-text">
-                                <a href="">Project Cirle-Solutions</a>
-                            </div>
-                            <div class="project-buttons">
-                                <a class="button" href="#">More Details</a>
-                                <a class="button" href="#">Live Demo</a>
-                            </div>
+                        <div class="project-text">
+                            <a href="">Project Cirle-Solutions</a>
+                        </div>
+                        <div class="project-buttons">
+                            <a class="button" href="#">More Details</a>
+                            <a class="button" href="#">Live Demo</a>
+                        </div>
                     </div>
                     <div class="grid-item">
                         <div class="placeholder-image">
-                        <!-- <img src="" loading="lazy" alt=""> -->
+                            <!-- <img src="" loading="lazy" alt=""> -->
                         </div>
-                            <div class="project-text">
-                                <a href="">Project</a>
-                            </div>
-                            <div class="project-buttons">
-                                <a class="button" href="#">More Details</a>
-                                <a class="button" href="#">Live Demo</a>
-                            </div>
+                        <div class="project-text">
+                            <a href="">Project</a>
+                        </div>
+                        <div class="project-buttons">
+                            <a class="button" href="#">More Details</a>
+                            <a class="button" href="#">Live Demo</a>
+                        </div>
                     </div>
                     <div class="grid-item">
                         <div class="placeholder-image">
-                        <!-- <img src="" loading="lazy" alt=""> -->
+                            <!-- <img src="" loading="lazy" alt=""> -->
                         </div>
-                            <div class="project-text">
-                                <a href="">Project</a>
-                            </div>
-                            <div class="project-buttons">
-                                <a class="button" href="#">More Details</a>
-                                <a class="button" href="#">Live Demo</a>
-                            </div>
+                        <div class="project-text">
+                            <a href="">Project</a>
+                        </div>
+                        <div class="project-buttons">
+                            <a class="button" href="#">More Details</a>
+                            <a class="button" href="#">Live Demo</a>
+                        </div>
                     </div>
                     <div class="grid-item">
                         <div class="placeholder-image">
-                        <!-- <img src="" loading="lazy" alt=""> -->
+                            <!-- <img src="" loading="lazy" alt=""> -->
                         </div>
-                            <div class="project-text">
-                                <a href="">Project</a>
-                            </div>
-                            <div class="project-buttons">
-                                <a class="button" href="#">More Details</a>
-                                <a class="button" href="#">Live Demo</a>
-                            </div>
+                        <div class="project-text">
+                            <a href="">Project</a>
+                        </div>
+                        <div class="project-buttons">
+                            <a class="button" href="#">More Details</a>
+                            <a class="button" href="#">Live Demo</a>
+                        </div>
                     </div>
                     <div class="grid-item">
                         <div class="placeholder-image">
-                        <img src="img/DVD-Video_Logo.svg.png" loading="lazy" alt="">
+                            <img src="img/DVD-Video_Logo.svg.png" loading="lazy" alt="">
                         </div>
-                            <div class="project-text">
-                                <a href="">Videotheek</a>
-                            </div>
-                            <div class="project-buttons">
-                                <a class="button" href="project5/videotheek.php">More Details</a>
-                            </div>
+                        <div class="project-text">
+                            <a href="">Videotheek</a>
+                        </div>
+                        <div class="project-buttons">
+                            <a class="button" href="project5/videotheek.php">More Details</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -129,22 +136,22 @@
             <div class="contact-content">
                 <div class="contact-form">
                     <div class="contact-text">
-                    <p>contact</p>
+                        <p>contact</p>
                     </div>
                     <form action="contact.php" method="post" class="contact-form">
                         <div class="contact-items">
                             <div class="text-input">
                                 <input type="text" name="name" placeholder="Enter your name:" required>
                             </div>
-    
+
                             <div class="text-input">
                                 <input type="email" name="email" placeholder="Enter your email:" required>
                             </div>
-    
+
                             <div class="text-input">
                                 <input type="text" name="subject" placeholder="Enter your subject:" required>
                             </div>
-    
+
                             <div class="contact-select">
                                 <select name="type" required>
                                     <option value="why">Reason for contact:</option>
@@ -153,9 +160,9 @@
                                     <option value="Other">Other</option>
                                 </select>
                             </div>
-    
+
                             <div class="contact-message">
-                                <textarea name="message"placeholder="Write your message:" required></textarea>
+                                <textarea name="message" placeholder="Write your message:" required></textarea>
                             </div>
                         </div>
                         <div class="contact-newsletter">
@@ -176,18 +183,16 @@
         <div class="opdracht-container">
             <div class="opdracht-content">
                 <div class="opdracht-text">
-                <?php 
-                    function vergelijkMet100($getal) {
+                    <?php
+                    function vergelijkMet100($getal)
+                    {
                         if ($getal === null) {
                             echo "Geen getal opgegeven.";
-                        }
-                        elseif ($getal > 100) {
+                        } elseif ($getal > 100) {
                             echo "$getal is hoger dan 100.";
-                        }
-                        elseif ($getal < 100) {
+                        } elseif ($getal < 100) {
                             echo "$getal is later dan 100.";
-                        }
-                        else {
+                        } else {
                             echo "$getal is gelijk aan 100.";
                         }
                     }
@@ -206,4 +211,5 @@
     <?php include("footer.php") ?>
 
 </body>
+
 </html>

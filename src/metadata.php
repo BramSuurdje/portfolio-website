68a5cd403d85f15093ae5f0786134b6a41de8004
Bram Suurd <bram.suurd@student.nhlstenden.com>
2023-12-25 13:52:36 +0100

    Update style.css link in metadata.php

--- src/metadata.php
+++ src/metadata.php
@@ -6,5 +6,5 @@
 <script src="https://unpkg.com/scrollreveal"></script>
 <script src="js/smoothscroll.js" defer></script>
 <meta name="theme-color" content="#222435">
-<link rel="stylesheet" href="/css/style.css?id=1234">
+<link rel="stylesheet" href="/css/style.css?id=1235">
 <script src="https://cdn.tailwindcss.com"></script>
